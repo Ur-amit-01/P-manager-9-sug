eee0522236df339a86e1822bba291c3b2069b64d
Amit <heroku467@gmail.com>
2026-05-02 00:05:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ logging.getLogger("pyrogram").setLevel(logging.ERROR)
 class Bot(Client):
     def __init__(self):
         super().__init__(
-            name=os.environ.get("SESSION_NAME", "renamer"),
+            name="renamer",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
